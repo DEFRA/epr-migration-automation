858b20c6779ebe10dcb264181b47bb77da6a594c
Build Service <buildservice@dev.azure.com>
2023-12-05 11:01:43 +0000

    Refreshed from ADO

--- pwsh/set-reponame.ps1
+++ pwsh/set-reponame.ps1
@@ -9,6 +9,8 @@ param(
     [Parameter(Mandatory)]
     [string]$NewName,
     [Parameter()]
+    [string]$AdoOutputVariableName,
+    [Parameter()]
     [string]$AccessToken
 )
 
@@ -34,6 +36,7 @@ Write-Debug "${functionName}:OrganizationUri=$OrganizationUri"
 Write-Debug "${functionName}:Project=$Project"
 Write-Debug "${functionName}:Repo=$Repo"
 Write-Debug "${functionName}:NewName=$NewName"
+Write-Debug "${functionName}:AdoOutputVariableName=$AdoOutputVariableName"
 
 [System.IO.DirectoryInfo]$scriptDir = $PSCommandPath | Split-Path -Parent
 Write-Debug "${functionName}:scriptDir.FullName=$scriptDir.FullName"
@@ -47,8 +50,20 @@ try {
   Initialize-AdoCli -OrganizationUri $OrganizationUri -Project $Project -AccessToken $AccessToken
 
   Write-Host "Renaming $Repo to $NewName"
-  Set-AdoRepoName -Repo $Repo -NewName $NewName | Write-Output
 
+  if ([string]::IsNullOrEmpty($AdoOutputVariableName)) {
+    Set-AdoRepoName -Repo $Repo -NewName $NewName | Write-Output
+  }
+  else {
+    [hashtable]$outputModel = @{
+        "oldName" = $Repo
+        "newName" = $NewName
+    }
+
+    [string]$output = $outputModel | ConvertTo-Json -Compress 
+
+    Write-Host "##vso[task.setvariable variable=$AdoOutputVariableName;isoutput=true]$output"
+}
   $exitCode = 0
 }
 catch {
